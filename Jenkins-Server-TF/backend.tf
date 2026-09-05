fc7345c5057b78cdc189fc5579d163f2323e714b
Nikhil <139710962+Nikhil-KVS@users.noreply.github.com>
2026-09-06 01:58:15 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "tetris-ews-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
+    use_lockfile   = true
     encrypt        = true
   }
   required_version = ">=0.13.0"
